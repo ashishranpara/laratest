ab0d401003834dea943de7ad9162d3046b321ab8
Manish Khilwani <pratham@Manishs-MacBook-Pro.local>
2015-11-05 15:50:55 +0530

    key changes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'uXONSG2woyi1lQqO1krX3w4BvOPnnRDK'),
 
     'cipher' => 'AES-256-CBC',
 
